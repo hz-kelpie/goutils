c1629130b8855a19660cc3960f8f0d832beccd16
zhongqichao <zhongqichao@corp.netease.com>
2021-09-24 10:13:23 +0800

    add expire map utils

---
+++ maputils/expiremap_test.go
@@ -0,0 +1,41 @@
+package maputils
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func Test_Map(t *testing.T) {
+	cache := NewExpiredMap()
+
+	for i := 1; i <= 10; i++ {
+		cache.Set(i, i, int64(i))
+	}
+	cache.Delete(8)
+	cache.Delete(9)
+	cache.Delete(10)
+	for i := 1; i <= 100; i++ {
+		cache.Set(10+i, 8, 5)
+	}
+	for i := 1; i <= 10; i++ {
+		cache.DoForEach(foreach)
+		fmt.Println("==========")
+		time.Sleep(time.Second)
+	}
+	cache.Close()
+	time.Sleep(time.Millisecond)
+	for i := 0; i < 1000; i++ {
+		time.Sleep(time.Millisecond * 1)
+		cache.Set(i, i, int64(i))
+		fmt.Println(cache.Get(i))
+	}
+}
+
+func foreach(key interface{}, val interface{}) {
+	k := key.(int)
+	if k > 10 {
+		return
+	}
+	fmt.Println("key", k, "val", val)
+}
